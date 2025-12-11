597f567affeebfda13f337c6842d3086171cfd64
bieskholodov <99995627+bieskholodov@users.noreply.github.com>
2025-12-11 06:01:06 +0200

    Update ms01.h

--- components/mcp41010/ms01.h
+++ components/mcp41010/ms01.h
@@ -5,25 +5,40 @@
 #include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
-namespace ms01 { 
+namespace ms01 {
 
-// Наследуемся от cg::Component и PollingComponent
+/**
+ * Компонент для работы с датчиком MS01 (аналог DHTxx).
+ * Реализует чтение влажности через GPIO с периодическим опросом.
+ */
 class MS01Component : public Component, public PollingComponent {
  public:
+  /**
+   * Устанавливает пин для подключения датчика.
+   * @param pin Указатель на объект GPIOPin
+   */
   void set_pin(GPIOPin *pin) { pin_ = pin; }
-  
-  // Объявляем сенсоры публичными, чтобы их можно было создать в setup()
+
+  /**
+   * Сенсор влажности. Может быть сконфигурирован во внешнем YAML.
+   */
   sensor::Sensor *humidity_sensor_{nullptr};
 
+  // Методы жизненного цикла компонента
   void setup() override;
   void update() override;
 
  protected:
+  /**
+   * Основной метод чтения данных с датчика по протоколу DHT (Tasmota-логика).
+   * @return Значение влажности в процентах или NAN при ошибке
+   */
   float readDHT_tasmota_logic();
 
  private:
-    GPIOPin *pin_;
+  GPIOPin *pin_;  ///< Пин подключения датчика
 };
 
 }  // namespace ms01
 }  // namespace esphome
+
