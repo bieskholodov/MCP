a0ee33cf5d633ac3f0554801e34c02d63ad98601
bieskholodov <99995627+bieskholodov@users.noreply.github.com>
2025-12-11 06:10:58 +0200

    Update __init__.py

--- components/mcp41010/__init__.py
+++ components/mcp41010/__init__.py
@@ -1,27 +1,25 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_PIN
+from esphome.components import gpio  # Добавляем импорт для работы с пинами
 
 ms01_ns = cg.esphome_ns.namespace("ms01")
-# Наследуемся от cg.Component, как в рабочем примере MCP41010
 MS01Component = ms01_ns.class_("MS01Component", cg.Component)
 
-CONFIG_SCHEMA = ( 
-    cv.Schema({    
+CONFIG_SCHEMA = (
+    cv.Schema({
         cv.GenerateID(): cv.declare_id(MS01Component),
-        cv.Required(CONF_PIN): cv.int_, # Обязательный пин для данных (ожидается целое число)
+        cv.Required(CONF_PIN): gpio.gpio_pin_schema,  # Используем валидатор из gpio
     })
-    .extend(cv.COMPONENT_SCHEMA) # Используем базовую схему компонента
-    # Опрос и сенсоры определяются в C++ коде
+    .extend(cv.COMPONENT_SCHEMA)
 )
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    
-    # ИСПРАВЛЕНИЕ: Передаем номер пина напрямую в функцию set_pin() C++ класса.
-    # Эта функция ожидает int, который мы получаем из config[CONF_PIN].
-    cg.add(var.set_pin(config[CONF_PIN]))
-    
-    # С этого момента данные будут отправлены из C++ кода компоненту-шаблону.
 
+    # Создаём объект GPIOPin из конфигурации
+    pin = await gpio.new_gpio_pin(config[CONF_PIN])
+    
+    # Передаем указатель на GPIOPin в метод set_pin()
+    cg.add(var.set_pin(pin))
